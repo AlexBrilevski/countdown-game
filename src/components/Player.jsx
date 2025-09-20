e600a1815b78a4f31f0880d55d88c6f982372a0a
Alex Brilevski <brilevski.alex@gmail.com>
2025-09-20 11:27:09 +0300

    Player component was refactored using refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Player() {
+  const nameInput = useRef();
   const [playerName, setPlayerName] = useState('');
-  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleInputChange = (e) => {
-    setIsSubmitted(false);
-    setPlayerName(e.currentTarget.value);
-  };
   const handleButtonClick = () => {
-    setIsSubmitted(true);
+    setPlayerName(nameInput.current.value);
+    nameInput.current.value = '';
   };
 
   return (
     <section id="player">
-      <h2>Welcome {isSubmitted ? playerName : 'unknown entity'}</h2>
+      <h2>Welcome {playerName ? playerName : 'unknown entity'}</h2>
       <p>
-        <input value={playerName} onChange={handleInputChange} type="text" />
+        <input ref={nameInput} type="text" />
         <button onClick={handleButtonClick}>Set Name</button>
       </p>
     </section>
