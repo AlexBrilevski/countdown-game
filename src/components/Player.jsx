24e9489acd4f9de586e9b5ae5da73a20294c24a2
Alex Brilevski <brilevski.alex@gmail.com>
2025-09-20 10:49:48 +0300

    Setting player name was added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.currentTarget.value);
+  };
+  const handleButtonClick = () => {
+    setPlayerName(inputValue);
+    setInputValue('');
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input value={inputValue} onChange={handleInputChange} type="text" />
+        <button onClick={handleButtonClick}>Set Name</button>
       </p>
     </section>
   );
