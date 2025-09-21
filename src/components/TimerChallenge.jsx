86ac0a997a8e7f7d6f5072e9e2638bb6b77e2243
Alex Brilevski <brilevski.alex@gmail.com>
2025-09-21 18:41:16 +0300

    Logic of starting and stopping challenge was added

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,38 @@
-export default function TimerChallenge({title, targetTime}) {
-    return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? 's' : ''}
-            </p>
-            <p>
-                <button>
-                    Start challenge
-                </button>
-            </p>
-            <p>
-                Timer is running... / Timer inavtive
-            </p>
-        </section>
-    );
+import { useState, useRef } from 'react';
+
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerIsStarted, setTimerIsStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const timer = useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerIsStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerIsStarted(false);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired ? <p>You lost!</p> : ''}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button onClick={timerIsStarted ? handleStop : handleStart}>
+          {timerIsStarted ? 'Stop' : 'Start'} challenge
+        </button>
+      </p>
+      <p>
+        {timerIsStarted ? 'Timer is running...' : 'Timer inavtive'}
+      </p>
+    </section>
+  );
 }
