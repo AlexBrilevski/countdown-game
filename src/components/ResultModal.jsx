04594058d20ea5b2332a0454c7d8730c52721f8b
Alex Brilevski <brilevski.alex@gmail.com>
2025-09-27 09:43:04 +0300

    Outputting score was added to the result modal component

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -5,6 +5,7 @@ const ResultModal = forwardRef(({ timeRemaining, targetTime, onReset }, ref) => 
 
   const userLost = timeRemaining <= 0;
   const formattedTime = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -16,7 +17,7 @@ const ResultModal = forwardRef(({ timeRemaining, targetTime, onReset }, ref) => 
 
   return (
     <dialog ref={dialog} className="result-modal">
-      {userLost && <h2>You lost</h2>}
+      <h2>{userLost ? 'You lost' : `Your Score: ${score}`}</h2>
       <p>The target time was <strong>{targetTime} seconds</strong>.</p>
       <p>You stopped the timer with <strong>{formattedTime} seconds left</strong>.</p>
       <form method="dialog" onSubmit={onReset}>
